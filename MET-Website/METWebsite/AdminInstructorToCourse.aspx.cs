19faf18f667cccde861b6f7e95956ac6bdccc243
mohamed Hamdy <mohamed.hamdyy83@gmail.com>
2023-06-26 16:10:40 +0300

    finished

---
+++ MET-Website/METWebsite/AdminInstructorToCourse.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+
+namespace METWebsite
+{
+    public partial class AdminInstructorToCourse : System.Web.UI.Page
+    {
+        protected void page_load(object sender, EventArgs e) 
+        {
+            string strcon = System.Configuration.ConfigurationManager.ConnectionStrings["MET"].ConnectionString;
+            SqlConnection con = new SqlConnection(strcon);
+            con.Open();
+            SqlCommand getcourses = new SqlCommand("select serial,code,name from Course", con);
+            SqlDataReader reader = getcourses.ExecuteReader();
+            string serial = ""; string name = ""; string code = "";
+            while (reader.Read())
+            {
+                serial = reader.GetValue(0).ToString();
+                Response.Write(serial);
+                name = reader.GetValue(2).ToString();
+                code = reader.GetValue(1).ToString();
+
+                var citem = new HtmlGenericControl("div");
+                citem.Attributes.Add("class", "courseItem");
+
+                var clabeldiv = new HtmlGenericControl("div");
+                clabeldiv.Attributes.Add("class", "courseLabel");
+
+                var clabel = new HtmlGenericControl("div");
+                clabel.Attributes.Add("class", "courseName");
+                clabel.InnerHtml = "(" + code + ") " + name;
+
+                var cbut = new HtmlGenericControl("button");
+               // cbut.ID = serial;
+                cbut.Attributes.Add("class", "select");
+                cbut.Attributes.Add("runat", "server");
+                cbut.InnerHtml = "Select";
+                //cbut.Attributes.Add("onclientclick ", "selectedcourse()");
+
+
+                clabeldiv.Controls.Add(clabel);
+                citem.Controls.Add(clabeldiv);
+                citem.Controls.Add(cbut);
+                coursesList.Controls.Add(citem);
+           
+
+            }
+       
+            con.Close();
+        }
+
+
+
+
+
+    }
+}
---
+++ MET-Website/METWebsite/StaffProfile.aspx
@@ -0,0 +1,49 @@
+﻿
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Staff</title>
+    <link  rel="stylesheet" href="./StyleSheets/SecondPage.css"/>
+</head>
+<body>
+    <div id="back" class="backG">
+  <%--      <div id="imagediv">
+
+
+        </div>--%>
+        <div id="textdiv">
+            <p id="p1">WHAT WE OFFER?</p>
+            <p id="p2">Our Mobile-App is facilitating  your supply chain 
+& offering  you the right Recipe for 
+a successful gastronomy business</p>
+            
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+    </div> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
